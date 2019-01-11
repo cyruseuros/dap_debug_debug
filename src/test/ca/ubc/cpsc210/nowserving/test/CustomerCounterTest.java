4d84e7c475c2f32f411a97cf97b460fcdacdaba3
Uros Perisic <uros.m.perisic@gmail.com>
2019-01-11 12:11:01 -0800

    Added class level comment to Customer Counter Test.

--- src/test/ca/ubc/cpsc210/nowserving/test/CustomerCounterTest.java
+++ src/test/ca/ubc/cpsc210/nowserving/test/CustomerCounterTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+// unit tests for the consumer counter class
 class CustomerCounterTest {
     private CustomerCounter testCounter;
 
